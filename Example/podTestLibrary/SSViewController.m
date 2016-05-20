0cf56511d21f728585ad5b4154e2b5eeb97717c1
jiuhao-yangshuo <410900345@qq.com>
2016-05-20 13:56:01 +0800

    Initial commit

---
+++ Example/podTestLibrary/SSViewController.m
@@ -0,0 +1,29 @@
+//
+//  SSViewController.m
+//  podTestLibrary
+//
+//  Created by jiuhao-yangshuo on 05/20/2016.
+//  Copyright (c) 2016 jiuhao-yangshuo. All rights reserved.
+//
+
+#import "SSViewController.h"
+
+@interface SSViewController ()
+
+@end
+
+@implementation SSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/podTestLibrary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  podTestLibrary
+//
+//  Created by jiuhao-yangshuo on 05/20/2016.
+//  Copyright (c) 2016 jiuhao-yangshuo. All rights reserved.
+//
+
+@import UIKit;
+#import "SSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
